082beab863f215cc684bad04874258781b964837
Danny-Bui <dannybui@live.se>
2024-06-14 21:16:27 +0200

    Added interactables: Levers with barriers

---
+++ Assets/Scripts/Interactables/Lever.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lever : MonoBehaviour, Interactable
+{
+    [SerializeField] private List<Barrier> barriers;
+
+    public void Interact()
+    {
+        Debug.Log("Interacting with lever");
+
+        foreach (Barrier barrier in barriers)
+        {
+            barrier.ToggleBarrier();
+        }
+    }
+}
