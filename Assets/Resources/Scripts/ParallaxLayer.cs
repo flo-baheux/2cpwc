ecc0bc0c1b19faaadf8ec961efa76768b5098709
Laywelin - Florian Baheux <florian.baheux@gmail.com>
2024-06-15 10:52:54 +0200

    Add parallax

---
+++ Assets/Resources/Scripts/ParallaxLayer.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class ParallaxLayer : MonoBehaviour
+{
+  /// <summary>
+  /// Movement of the layer is scaled by this value.
+  /// </summary>
+  public Vector3 movementScale = Vector3.one;
+
+  Transform _camera;
+
+  void Awake()
+  {
+    _camera = Camera.main.transform;
+  }
+
+  void LateUpdate()
+  {
+    transform.position = Vector3.Scale(_camera.position, movementScale);
+  }
+
+}
