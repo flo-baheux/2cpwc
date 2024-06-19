bbd187b952e66989c6016a2f1a6ce28c81ce86a3
Miruna-Ioana Stanescu <64460287+CuppycakeFighter@users.noreply.github.com>
2024-06-19 14:44:25 +0300

    Levels2,3,4 (#23)
    * Particle pack
    
    * Full Level2 and checkpoints on Level1
    
    * Level3
    
    * Level 4
    
    * Level4
    
    * Level1 tweaks
    
    * Fix Climbing
    
    * Level 2 Tweaks
    
    * Menu Tweak
    
    ---------
    
    Co-authored-by: Laywelin - Florian Baheux <florian.baheux@gmail.com>

--- Assets/Resources/Scripts/Character/Player/States/PlayerClimbingState.cs
+++ Assets/Resources/Scripts/Character/Player/States/PlayerClimbingState.cs
@@ -1,42 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerClimbingState : PlayerState
 {
-        
-    public PlayerClimbingState(Player player) : base(player)
+
+  public PlayerClimbingState(Player player) : base(player)
+  {
+    this.state = State.CLIMBING;
+  }
+
+  public override void Enter()
+  {
+    Player.rigidBody.isKinematic = true;
+    Player.rigidBody.velocity = Vector2.zero;
+    Player.transform.position = Player.Climbable.transform.GetChild(0).position;
+  }
+
+  public override State? CustomUpdate()
+  {
+    if (Player.controlsEnabled && Player.playerInput.actions["Jump"].WasPressedThisFrame())
     {
-        this.state = State.CLIMBING;
-    }
-    
-    public override void Enter()
-    {
-        Player.rigidBody.isKinematic = true;
-        Player.rigidBody.velocity = Vector2.zero;
-        Player.transform.position = Player.Climbable.transform.position;
+      Player.rigidBody.isKinematic = false;
+      Player.transform.position = Player.Climbable.transform.GetChild(1).position;
+      return State.JUMPING;
     }
 
-    public override State? CustomUpdate()
+    if (Player.controlsEnabled && Player.playerInput.actions["DropDown"].WasPressedThisFrame())
     {
-        if (Player.controlsEnabled && Player.playerInput.actions["Jump"].WasPressedThisFrame())
-        {
-            Player.rigidBody.isKinematic = false;
-            Player.rigidBody.AddForce(Player.jumpHeight * 7f * Vector2.up, ForceMode2D.Impulse);
-            return State.JUMPING;
-        }
-        
-        if (Player.controlsEnabled && Player.playerInput.actions["DropDown"].WasPressedThisFrame())
-        {
-            Player.rigidBody.isKinematic = false;
-            return State.JUMPING;
-        }
-        
-        return null;
+      Player.rigidBody.isKinematic = false;
+      return State.JUMPING;
     }
 
-    public override void Exit()
-    {
-        
-    }
+    return null;
+  }
+
+  public override void Exit()
+  {
+
+  }
 }
